4c50f83e3267b737b7f88d4a4cc7ea7ae1a05a98
minthukha1288 <minthukha1239788@gmail.com>
2023-09-12 12:47:56 +0630

    Admin Pannel

--- src/page/create-blogs/index.jsx
+++ src/page/create-blogs/index.jsx
@@ -1,57 +1,68 @@
 import { useState } from "react";
 import Button from "../../components/button/Button";
+import styles from "./style.module.css";
 
 function CreateBlogs() {
   const [Form, setForm] = useState({
-    body:"",
-    author:"",
-    title:""
-  })
+    body: "",
+    author: "",
+    title: "",
+  });
   const ChangeHandaler = (e) => {
     console.log(e.target.value);
   };
   const FileHandaler = (e) => {
-    console.log("File")
-  }
+    console.log("File");
+  };
   return (
-    <form>
-      <label htmlFor="title">Title</label>
-      <input
-        id="title"
-        type="text"
-        name="title"
-        placeholder="Enter Titel"
-        value={``}
-        onClick={ChangeHandaler}
-      />
-      <label htmlFor="author">Author</label>
-      <input
-        id="author"
-        type="text"
-        name="author"
-        placeholder="Enter Author"
-        value={``}
-        onClick={ChangeHandaler}
-      />
-      <label htmlFor="body">Body</label>
-      <input
-        id="body"
-        type="text"
-        name="body"
-        placeholder="Enter Body"
-        value={``}
-        onClick={ChangeHandaler}
-      />
-      <label htmlFor="image">Select Image</label>
-      <input
-        id="image"
-        type="file"
-        name="Image"
-        value={``}
-        onClick={FileHandaler}
-        hidden={true}
-      />
-      <button>Submit</button>
+    <form className={styles.formConatainner}>
+      <div className={styles.inputContainner}>
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          placeholder="Enter Titel"
+          value={``}
+          className={styles.input}
+          onClick={ChangeHandaler}
+        />
+      </div>
+      <div className={styles.inputContainner}>
+        <label htmlFor="author">Author</label> <br />
+        <input
+          id="author"
+          type="text"
+          name="author"
+          placeholder="Enter Author"
+          value={``}
+          className={styles.input}
+          onClick={ChangeHandaler}
+        />
+      </div>
+      <div className={styles.inputContainner}>
+        <label htmlFor="image" className={styles.selectImg}>
+          Select Image
+        </label>{" "}
+        <br />
+        <input
+          id="image"
+          type="file"
+          name="Image"
+          value={``}
+          onClick={FileHandaler}
+          hidden={true}
+        />
+      </div>
+      <div className={styles.inputContainner}>
+        <textarea
+          name="body"
+          id="body"
+          placeholder="Enter body text"
+          className={styles.textarea}
+        ></textarea>
+      </div>
+      <button className={styles.submitBtn}>Submit</button>
     </form>
   );
 }
